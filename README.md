02fb019299447468003e3ebf6a2453c2eda6bcb4
zwhGithub <452216418@qq.com>
2018-01-13 12:06:51 +0800

    修改了翻页距离

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 ## vue-swiper
 - 基于Vue2.0开发，基本满足大部分功能
 - 轻量、高性能轮播插件。目前支持  无缝衔接自动轮播、无限轮播、手势轮播
-- 打包之后只有8.2KB大小 性能还是杠杠滴
+- 没有引入第三方库，原生js封装,打包之后只有8.2KB大小 性能还是杠杠滴
 
 
 
 ## demo
 
  ![效果](http://zwhgithub.github.io/vue-swiper/dist/1514291260.png)
- 
+
  - 如果觉得好用，给一个star哦~~~
 
 ## Install
@@ -51,7 +51,7 @@ components: {
     //加一些参数配置情况
   <Swiper v-if="slidesReal.length > 0"
   		  :autoPlay='true' //是否自动轮播
-       @transtionend='move' 
+       @transtionend='move'
        //每次轮播之后调用的方法，参数是跳到第几个轮播图
   		  :showIndicator='true' //是否显示轮播的那个点
   		  interval="2500" //每两次隔多久滚动一次
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,38 +1,36 @@
 <template>
-    <div class="hello">
-        <Swiper v-if="list.length>0">
-            <Slide v-for="(tag,key) in list" :key=key>
-                <img :src=tag />
-            </Slide>
-        </Swiper>
-    </div>
+  <div class="hello">
+    <Swiper v-if="list.length>0">
+      <Slide v-for="(tag,key) in list" :key=key>
+        <img :src="tag.img" />
+      </Slide>
+    </Swiper>
+  </div>
 </template>
 
 <script>
 import { Swiper, Slide } from 'vue-swiper-component';
 // import { Swiper, Slide } from './index.js';
-// import Swiper from './swiper';
-// import Slide from './slide';
 export default {
-    data() {
-        return {
-            list: [
-                'https://qiniu.epipe.cn/5456575529551388672?imageslim&imageView2/1/w/750/h/360',
-                'https://qiniu.epipe.cn/5430983074181545984?imageslim&imageView2/1/w/750/h/360',
-                'https://qiniu.epipe.cn/5464226412548325376?imageslim&imageView2/1/w/750/h/360'
-            ]
-        };
-    },
-    components: {
-        Swiper,
-        Slide
-    }
+  data() {
+    return {
+      list: [
+        { img: 'https://qiniu.epipe.cn/5456575529551388672?imageslim&imageView2/1/w/750/h/360' },
+        { img: 'https://qiniu.epipe.cn/5430983074181545984?imageslim&imageView2/1/w/750/h/360' },
+        { img: 'https://qiniu.epipe.cn/5464226412548325376?imageslim&imageView2/1/w/750/h/360' }
+      ]
+    };
+  },
+  components: {
+    Swiper,
+    Slide
+  }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 img {
-    width: 100%;
+  width: 100%;
 }
 </style>
